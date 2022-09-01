32306023a3f8850aa2fa2e32ae5740f7621c19d2
Larocque240 <64595156+Larocque240@users.noreply.github.com>
2022-08-31 19:59:48 -0500

    Add files via upload

---
+++ Quintrix_Assignment_4/QuintrixAssignment4Application.java
@@ -0,0 +1,13 @@
+package com.example.Quintrix_Assignment_4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuintrixAssignment4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuintrixAssignment4Application.class, args);
+	}
+
+}
---
+++ Quintrix_Assignment_4/controller/TextTransfer.java
@@ -0,0 +1,19 @@
+package com.example.Quintrix_Assignment_4.controller;
+
+public class TextTransfer {
+
+		private String text;
+
+		public TextTransfer(String string) {
+			string = text;
+		}
+
+		public String getText() {
+			return text;
+		}
+
+		public void setText(String text) {
+			this.text = text;
+		}
+		
+}
