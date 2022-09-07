2b6e8a70ecca0bd7fdd19106f494c31320b2f46f
Larocque240 <64595156+Larocque240@users.noreply.github.com>
2022-09-07 17:23:48 -0500

    Add files via upload

---
+++ RestMain/src/main/java/com/example/restmain/HelloController.java
@@ -0,0 +1,24 @@
+package com.example.restmain;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class HelloController {
+
+	@Autowired
+	RestTemplate restTemplate;
+	
+	@GetMapping("/home")
+	public String home() {
+		
+		ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8080/getusers", String.class);
+		return response.getBody();
+	}
+		
+	
+	
+}
---
+++ RestMain/src/test/java/com/example/restmain/RestMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restmain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
