af7c8cd21fe3e3e4bc4b003783e2f0ab60b62595
Larocque240 <64595156+Larocque240@users.noreply.github.com>
2022-09-07 17:43:32 -0500

    Add files via upload

--- RestMain/src/main/java/com/example/restmain/HelloController.java
+++ RestMain/src/main/java/com/example/restmain/HelloController.java
@@ -1,10 +1,15 @@
 package com.example.restmain;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import com.example.restmain.Users;
 
 @RestController
 public class HelloController {
@@ -12,13 +17,17 @@ public class HelloController {
 	@Autowired
 	RestTemplate restTemplate;
 	
-	@GetMapping("/home")
-	public String home() {
+	@GetMapping("/get")
+	public String getuser() {
 		
 		ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8080/getusers", String.class);
 		return response.getBody();
 	}
 		
-	
+	@PostMapping
+	public String postuser(@RequestBody Users User) {
+		HttpEntity<Users> response = new HttpEntity<>(User);
+		return restTemplate.postForObject("http://localhost:8080/user", response, String.class);
+	}
 	
 }
---
+++ RestMain/src/main/java/com/quintrix/java/userdb/UserdbApplication.java
@@ -0,0 +1,20 @@
+package com.quintrix.java.userdb;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserdbApplication.class, args);
+		
+		// List<Integer> arr = new ArrayList();
+		 // arr.add(1);
+		 // arr.add(2);
+	}
+
+}
